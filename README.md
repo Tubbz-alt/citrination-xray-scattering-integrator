a6fafdd9598b24326a4498a0649313a1bd033c6f
Lenson Pellouchoud <lensonp@gmail.com>
2017-10-17 13:24:52 -0700

    renamed

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# citrination-integrator
-An ingester for the Citrination platform. Integrates a .tif image and produces a PIF record containing labeled intensity data.
+# citrination-xray-scattering-integrator
+An ingester (ingestor?) for the Citrination platform. 
+Integrates a .tif image and produces a PIF record containing labeled intensity data.
+
+The integration parameters are taken as input,
+in the PONI (point of normal incidence) format,
+as developed by ESRF's silx project 
+(https://github.com/silx-kit/pyFAI).
+
+The processing workflow is built on PAWS,
+the Platform for Automated Workflows by SSRL
+(https://github.com/slaclab/paws).
+
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='citrination_integrator',
+setup(name='citrination-xray-scattering-integrator',
     version='0.0.1',
-    url='https://github.com/slaclab/citrination_integrator.git`',
+    url='https://github.com/slaclab/citrination-xray-scattering-integrator.git`',
     description='integrates .tif images, produces PIF records from them',
     author='Lenson A. Pellouchoud',
     author_email='lenson@slac.stanford.edu',
@@ -12,7 +12,7 @@ setup(name='citrination_integrator',
     ],
     entry_points={
         'citrine.dice.converter': [
-            'scattering_integrator = citrination_integrator.integrate',
+            'xray_scattering_integrator = xray_scattering_integrator.integrate',
         ],
     },
 )
